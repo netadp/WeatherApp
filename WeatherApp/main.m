a5ae76162fe17975b68ffd66dbf416f6465a72aa
Huo <limen.huo@gmail.com>
2014-02-18 22:24:34 +0800

    Initial Commit

---
+++ WeatherApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeatherApp
+//
+//  Created by Jie Huo on 18/2/14.
+//  Copyright (c) 2014 Jie Huo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([WPAppDelegate class]));
+	}
+}
