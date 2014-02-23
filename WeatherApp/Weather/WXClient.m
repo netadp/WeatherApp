685705c1a8dbc48d68f49e81f455abb2cd2571e6
Huo <limen.huo@gmail.com>
2014-02-23 23:19:21 +0800

    finished 1.0

--- WeatherApp/Weather/WXClient.m
+++ WeatherApp/Weather/WXClient.m
@@ -7,7 +7,107 @@
 //
 
 #import "WXClient.h"
+#import "WXCondition.h"
+#import "WXDailyForcast.h"
+
+@interface WXClient ()
+@property (nonatomic, strong) NSURLSession *session;
+@end
 
 @implementation WXClient
+
+- (id)init
+{
+	if (self = [super init]) {
+		NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+		_session = [NSURLSession sessionWithConfiguration:config];
+	}
+	return self;
+}
+
+- (RACSignal *)fetchJSONFromURL:(NSURL *)url
+{
+	NSLog(@"Fetching: %@", url.absoluteString);
+	
+	// Returns the signal.
+	// This method creates an object for other methods and objects to use; This method is sometimes called the
+	// factory pattern.
+	return [[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+		NSURLSessionDataTask *dataTask = [self.session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+			// TODO:Handle retrieved data
+			if (!error) {
+				NSError *jsonError = nil;
+				id json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&jsonError];
+				if (!jsonError) {
+					[subscriber sendNext:json];
+				}
+				else{
+					[subscriber sendError:jsonError];
+				}
+			}
+			else{
+				[subscriber sendError:error];
+			}
+			
+			[subscriber sendCompleted];
+		}];
+		
+		// starts the network request once someone subscribes to the signal.
+		[dataTask resume];
+		
+		// Creates and returns an RACDisposable object which handles any cleanup when the signal is destroyed.
+		return [RACDisposable disposableWithBlock:^{
+			[dataTask cancel];
+		}];
+	}] doError:^(NSError *error) {
+		// Add a side effect to log any errors that occur. Sidde effects don't subscribe to the signal;
+		// Rather, they return the signal to which they're attached for method chaining.
+		NSLog(@"%@",error);
+	}];
+}
+
+- (RACSignal *)fetchCurrentConditionsForLocation:(CLLocationCoordinate2D)coordinate
+{
+	NSString *string = [NSString stringWithFormat:@"http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=imperial",coordinate.latitude, coordinate.longitude];
+	NSURL *url = [NSURL URLWithString:string];
+	
+	return [[self fetchJSONFromURL:url] map:^(NSDictionary *json) {
+		return [MTLJSONAdapter modelOfClass:[WXCondition class] fromJSONDictionary:json error:nil];
+	}];
+}
+
+- (RACSignal *)fetchHourlyForecastForLocation:(CLLocationCoordinate2D)coordinate {
+    NSString *urlString = [NSString stringWithFormat:@"http://api.openweathermap.org/data/2.5/forecast?lat=%f&lon=%f&units=imperial&cnt=12",coordinate.latitude, coordinate.longitude];
+    NSURL *url = [NSURL URLWithString:urlString];
+	
+    // 1
+    return [[self fetchJSONFromURL:url] map:^(NSDictionary *json) {
+        // 2
+        RACSequence *list = [json[@"list"] rac_sequence];
+		
+        // 3
+        return [[list map:^(NSDictionary *item) {
+            // 4
+            return [MTLJSONAdapter modelOfClass:[WXCondition class] fromJSONDictionary:item error:nil];
+			// 5
+        }] array];
+    }];
+}
+
+- (RACSignal *)fetchDailyForecastForLocation:(CLLocationCoordinate2D)coordinate {
+    NSString *urlString = [NSString stringWithFormat:@"http://api.openweathermap.org/data/2.5/forecast/daily?lat=%f&lon=%f&units=imperial&cnt=7",coordinate.latitude, coordinate.longitude];
+    NSURL *url = [NSURL URLWithString:urlString];
+	
+    // Use the generic fetch method and map results to convert into an array of Mantle objects
+    return [[self fetchJSONFromURL:url] map:^(NSDictionary *json) {
+        // Build a sequence from the list of raw JSON
+        RACSequence *list = [json[@"list"] rac_sequence];
+		
+        // Use a function to map results from JSON to Mantle objects
+        return [[list map:^id (NSDictionary *item) {
+            return [MTLJSONAdapter modelOfClass:[WXDailyForcast class] fromJSONDictionary:item error:nil];
+        }] array];
+    }];
+}
 
 @end
--- WeatherApp/Weather/WXManager.m
+++ WeatherApp/Weather/WXManager.m
@@ -7,7 +7,102 @@
 //
 
 #import "WXManager.h"
+#import "WXClient.h"
+#import <TSMessages/TSMessage.h>
+
+@interface WXManager ()
+@property (nonatomic, strong, readwrite) WXCondition *currentCondition;
+@property (nonatomic, strong, readwrite) CLLocation *currentLocation;
+@property (nonatomic, strong, readwrite) NSArray *hourlyForecast;
+@property (nonatomic, strong, readwrite) NSArray *dailyForecast;
+
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, assign) BOOL isFirstUpdate;
+@property (nonatomic, strong) WXClient *client;
+@end
 
 @implementation WXManager
 
++(instancetype)sharedManager
+{
+	static id _sharedManager = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		_sharedManager = [[self alloc]init];
+	});
+	
+	return _sharedManager;
+}
+
+-(id)init
+{
+	if (self = [super init]) {
+		_locationManager = [[CLLocationManager alloc]init];
+		_locationManager.delegate = self;
+		
+		// handles all networking and data parsing
+		_client = [[WXClient alloc]init];
+		
+		[[[[RACObserve(self, currentLocation) ignore:nil]
+		   // if flattens the values and returns one object containing all three signals
+		   // in this way, three processes can be considered as a single unit of work.
+		 flattenMap:^RACStream *(CLLocation *newLocation) {
+			 NSLog(@"merge");
+			 return [RACSignal merge:@[[self updateCurrentConditions],[self updateDailyForecast], [self updateHourlyForecast]]];
+		 }]
+		  // Deliver the signal to subscribers on the main thread.
+		  deliverOn:[RACScheduler mainThreadScheduler]]
+		 // It's not a good practice to interact with the UI from inside your model
+		 subscribeError:^(NSError *error) {
+			 [TSMessage showNotificationWithTitle:@"Error" subtitle:@"There was a problem fetching the latest weather" type:TSMessageNotificationTypeError];
+		 }];
+	}
+	
+	return self;
+}
+
+-(void)findCurrentLocation
+{
+	self.isFirstUpdate = YES;
+	[self.locationManager startUpdatingLocation];
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+	// ignore the first location update because it is almost always cached
+	if (self.isFirstUpdate) {
+		self.isFirstUpdate = NO;
+		return;
+	}
+	
+	CLLocation *location = [locations lastObject];
+	
+	// once have a location with proper accuracy, stop further updates;
+	if (location.horizontalAccuracy > 0) {
+		// set the location key trrigers the RACObservable you set earlier in the init implementation.
+		self.currentLocation = location;
+		[self.locationManager stopUpdatingLocation];
+	}
+}
+
+-(RACSignal *)updateCurrentConditions
+{
+	return [[self.client fetchCurrentConditionsForLocation:self.currentLocation.coordinate] doNext:^(WXCondition *condition){
+		self.currentCondition = condition;
+	}];
+}
+
+-(RACSignal*)updateHourlyForecast
+{
+	return [[self.client fetchHourlyForecastForLocation:self.currentLocation.coordinate] doNext:^(NSArray *conditions){
+		self.hourlyForecast = conditions;
+	}];
+}
+
+-(RACSignal*)updateDailyForecast
+{
+	return [[self.client fetchDailyForecastForLocation:self.currentLocation.coordinate] doNext:^(NSArray *conditions){
+		self.dailyForecast = conditions;
+	}];
+}
 @end
